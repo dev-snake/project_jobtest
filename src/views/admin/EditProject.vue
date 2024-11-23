d73dc81e077cfa237458d83360a763f27037428e
Đặng Văn Hậu <121596451+dev-snake@users.noreply.github.com>
2024-11-23 23:10:29 +0700

    Merge branch 'main' of https://github.com/dev-snake/project_jobtest

--- src/views/admin/EditProject.vue
+++ src/views/admin/EditProject.vue
@@ -5,61 +5,60 @@ import {ref} from "vue";
 
 const router = useRouter()
 const techList = ref([
-      {
-        id: 1,
-        name: 'Javascript',
-      },
-      {
-        id: 3,
-        name: 'Typescript',
-      },
-      {
-        id: 2,
-        name: 'Node.js',
-      },
-      {
-        id: 4,
-        name: 'PHP',
-      },
-      {
-        id: 5,
-        name: 'HTML5',
-      },
-      {
-        id: 6,
-        name: 'CSS',
-      },
-      {
-        id: 7,
-        name: 'ReactJS',
-      },
-      {
-        id: 8,
-        name: 'Next.js',
-      },
-      {
-        id: 9,
-        name: 'TailwindCss',
-      },
-      {
-        id: 10,
-        name: 'AngularJS',
-      },
-      {
-        id: 11,
-        name: 'MongoDB',
-      },
-      {
-        id: 12,
-        name: 'Express',
-      },
-      {
-        id: 13,
-        name: 'MySQL',
+  {
+    id: 1,
+    name: 'Javascript',
+  },
+  {
+    id: 3,
+    name: 'Typescript',
+  },
+  {
+    id: 2,
+    name: 'Node.js',
+  },
+  {
+    id: 4,
+    name: 'PHP',
+  },
+  {
+    id: 5,
+    name: 'HTML5',
+  },
+  {
+    id: 6,
+    name: 'CSS',
+  },
+  {
+    id: 7,
+    name: 'ReactJS',
+  },
+  {
+    id: 8,
+    name: 'Next.js',
+  },
+  {
+    id: 9,
+    name: 'TailwindCss',
+  },
+  {
+    id: 10,
+    name: 'AngularJS',
+  },
+  {
+    id: 11,
+    name: 'MongoDB',
+  },
+  {
+    id: 12,
+    name: 'Express',
+  },
+  {
+    id: 13,
+    name: 'MySQL',
 
-      }
-    ]
-)
+  }
+]);
 </script>
 <template>
   <div class="flex justify-between my-4">
